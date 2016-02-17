ec127b86148010afe775cd28ac9e3a0302ece301
Amitai Burstein <amitai@gizra.com>
2016-02-17 21:54:40 +0200

    More work on the bar

--- src/elm/Timeline/View.elm
+++ src/elm/Timeline/View.elm
@@ -3,7 +3,7 @@ module Timeline.View where
 import Graphics.Input
 import Text exposing (fromString)
 import Graphics.Element exposing (color, layers, leftAligned, sizeOf)
-import Graphics.Collage exposing (collage, outlined, rect, solid, toForm, filled, move)
+import Graphics.Collage exposing (collage, outlined, rect, solid, toForm, filled, move, moveX)
 import Color exposing (blue, black, orange, yellow)
 
 import Html                        exposing (div, h2, fromElement, li, span, text, ul, Html)
@@ -45,22 +45,32 @@ viewItems address items =
           [ class "items" ]
           ( List.map viewItem items )
 
+
+-- Get all the bar forms
+getForms : List Graphics.Collage.Form
+getForms =
+  let
+    hand index =
+      rect 2 20
+        |> filled black
+        |> move (index * 80, -10)
+
+    hands direction =
+      let
+        index =
+          if direction == True then [0..4] else [-4..0]
+      in
+      List.map hand index
+
+    bar =
+      rect 800 40
+        |> filled blue
+  in
+    bar :: (hands True) ++ (hands False)
+
+
 viewBar : Html
 viewBar =
-  let
-    forms =
-      [ rect 800 40
-          |> filled blue
-
-      , rect 2 20
-          |> filled black
-          |> move (0, -10)
-
-      , rect 2 10
-          |> filled black
-          |> move (-20, -15)
-      ]
-  in
-    div
-      []
-      [ fromElement (collage 800 40 forms) ]
+  div
+    []
+    [ fromElement (collage 800 40 getForms) ]
