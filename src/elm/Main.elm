8dc443a00c0904b23b664932afa787e9b4d86646
Amitai Burstein <amitai@gizra.com>
2016-02-17 23:20:00 +0200

    More wiring

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,18 +3,23 @@ import App.Model as App exposing (Model)
 import App.Update exposing (init, update)
 import App.View exposing (view)
 
+import Timeline.Update exposing (Action)
+
+import Drag exposing (trackMany)
 import Effects exposing (Never)
 
 import StartApp
 import Task
 
+hover =
+    Signal.mailbox Nothing
 
 app =
   StartApp.start
     { init = init
     , update = update
     , view = view
-    , inputs = []
+    , inputs = [ Signal.map (App.Update.ChildTimelineAction <<Timeline.Update.Track) (trackMany Nothing hover.signal) ]
     }
 
 
