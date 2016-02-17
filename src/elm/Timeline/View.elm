2b8eb23c510bf000f9af227c020a7418916f4665
Amitai Burstein <amitai@gizra.com>
2016-02-17 21:06:15 +0200

    More work

--- src/elm/Timeline/View.elm
+++ src/elm/Timeline/View.elm
@@ -1,12 +1,12 @@
 module Timeline.View where
 
-import Html exposing (div, h2, text, Html)
-import Html.Attributes exposing (class)
+import Html                        exposing (div, h2, li, text, ul, Html)
+import Html.Attributes             exposing (class)
 
 -- Components
-
-import Timeline.Model as Timeline exposing (initialModel, Model)
-import Timeline.Update exposing (Action)
+import Item.Model      as Item     exposing (Item)
+import Timeline.Model  as Timeline exposing (initialModel, Model)
+import Timeline.Update             exposing (Action)
 
 
 view : Signal.Address Action -> Model -> Html
@@ -14,5 +14,9 @@ view address model =
   div
     [ class "timeline"]
     [ h2 [] [ text "Timeline" ]
-    , text "Timeline"
+    , ul [ class "items" ] (List.map viewItem model.items)
     ]
+
+viewItem : Item -> Html
+viewItem item =
+  li [] [ text item.label ]
