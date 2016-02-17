ec9ffdb44fd7a690eb6a155a2220aa54958e96a3
Amitai Burstein <amitai@gizra.com>
2016-02-17 23:08:56 +0200

    Add track

--- src/elm/Timeline/Update.elm
+++ src/elm/Timeline/Update.elm
@@ -1,6 +1,8 @@
 module Timeline.Update where
 
 import Dict exposing (insert)
+import Drag exposing (..)
+
 
 import Item.Model as Item exposing (Item)
 import Timeline.Model exposing (initialModel, Model)
@@ -8,6 +10,7 @@ import Timeline.Model exposing (initialModel, Model)
 type Action
   = AddItem Item
   | ToggleItemSelection Int Item Bool
+  | Track (Maybe ( Int, Drag.Action ))
 
 update : Action -> Model -> Model
 update action model =
@@ -29,3 +32,19 @@ update action model =
         items' = Dict.insert index item' model.items
       in
         { model | items = items' }
+
+    Track (Just ( i, Lift )) ->
+      model
+
+    Track (Just ( i, MoveBy ( dx, dy ) )) ->
+      model
+
+    Track (Just ( i, Release )) ->
+      model
+
+    Track _ ->
+      model
+
+
+moveBy ( dx, dy ) ( x, y ) =
+    ( x + toFloat dx, 0 )
